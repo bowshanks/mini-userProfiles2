38e827c54d993dfd09c1cc744d95d2891be68b42
Daniel Gardner <bowshanks@gmail.com>
2016-08-31 12:28:23 -0600

    finished

--- controller.js
+++ controller.js
@@ -1,10 +1,28 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
+  $scope.pageNum = 1;
+
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+  	mainService.getUsers($scope.pageNum).then(function(response){
+      $scope.totalPages = response.data.total_pages;
+      $scope.users = response.data.data;
+    })
   }
 
-
   $scope.getUsers();
+
+
+  $scope.prev = function() {
+    if($scope.pageNum > 1){
+      $scope.getUsers(this.pageNum--);
+    }
+  }
+
+  $scope.next = function() {
+    if ($scope.pageNum < $scope.totalPages){
+      $scope.getUsers(this.pageNum++);
+    }
+
+  }
 
 });
